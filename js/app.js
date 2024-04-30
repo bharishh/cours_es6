173ab70339d332b8964af607fa36dfbe10c1012a
bharishh <harish_01@icloud.com>
2024-04-30 11:22:21 +0200

    js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 
-{}
+
+(()=>{
+
+    console.log("hello JavaScript")
+
+    const nom="Javascript" // const = c'est une variable qui change jamais 
+    let prenom = "Harish" // let = variable qui peut evolue 
+    let phone =64843
+
+   console.log(prenom)
+   console.log(typeof phone)
+   
+})()
+
